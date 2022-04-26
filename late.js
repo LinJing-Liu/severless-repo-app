606a6fa7dd3bf3c9df38a73fffb603cba1ac9fc8
LinJing-Liu <linjingl93@gmail.com>
2022-04-26 00:01:57 -0400

    add time func

--- late.js
+++ late.js
@@ -1,1 +1,12 @@
 // start coding your function here!
+function running_late(date) {
+	var time = date.split(" ")[3].split(":")[0];
+	time = parseInt(time);
+	if (time >= 22)
+		return "It is late!";
+	return "It is still early!";
+}
+
+module.exports = {
+        running_late,
+};
